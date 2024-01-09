b0d7f2fe2204b6a43309dbd4517a700193898b0a
Anderson Clinicorp <developersanter@gmail.com>
2024-01-09 19:42:29 -0300

    feat: app run 5050

--- connection/firebird.js
+++ connection/firebird.js
@@ -1,59 +1,44 @@
-// const config = require("../../../config");
-
-// const query = "SELECT * FROM EMD101"
-// runQueryInFirebird("SELECT * FROM EMD101", "localhost", 3051, "/firebird/data/db.FDB", "SYSDBA", "123").catch((error) => {
-//   console.log(error)
-// }).then((response) => {
-//     console.log(response)
-// });
 
 const http = require('http');
 const firebird = require('node-firebird');
 
-// Configurações do banco de dados Firebird
 const dbConfig = {
   host: 'localhost',
-  port: 3051, // Porta padrão do Firebird
+  port: 3051,
   database: "/firebird/data/db.FDB",
   user: 'SYSDBA',
-  password: '123',
+  password: 'andersonTestHueHue',
 };
 
-// Crie um servidor HTTP simples
 const server = http.createServer((req, res) => {
-  // Estabeleça uma conexão com o banco de dados
 
 });
 
 firebird.attach(dbConfig, (err, db) => {
     if (err) {
-      console.error('Erro ao conectar ao banco de dados:', err);
+      console.error('Connection Error', err);
       res.writeHead(500, { 'Content-Type': 'text/plain' });
-      res.end('Erro ao conectar ao banco de dados.');
+      res.end('Connection error.');
       return;
     }
 
-    // Execute uma consulta SQL
-    db.query('SELECT * FROM EMD101', (err, result) => {
+    db.query('SELECT * FROM Test', (err, result) => {
         console.log(result)
       if (err) {
-        console.error('Erro ao executar a consulta:', err);
+        console.error('Error:', err);
         res.writeHead(500, { 'Content-Type': 'text/plain' });
-        res.end('Erro ao executar a consulta.');
+        res.end('Error query.');
         return;
       }
 
-      // Envie a resposta como JSON
       res.writeHead(200, { 'Content-Type': 'application/json' });
       res.end(JSON.stringify(result, null, 2));
 
-      // Encerre a conexão com o banco de dados
       db.detach();
     });
   });
 
-// Defina a porta em que o servidor irá ouvir
-const port = 3000;
+const port = 5050;
 server.listen(port, () => {
-  console.log(`Servidor Node.js está ouvindo na porta ${port}`);
+  console.log(`Server: ${port}`);
 });
