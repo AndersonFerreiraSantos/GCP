ffce67c4c137092a13b35f7686f90cc4a7039b56
Anderson Clinicorp <developersanter@gmail.com>
2024-05-15 09:15:26 -0300

    test: up

--- fileServer/server.js
+++ fileServer/server.js
@@ -1,16 +1,31 @@
 const handler = require('serve-handler');
 const http = require('http');
 
-const PORT = 9000;
+const PORT = 3000;
 
 const options = {
-  public: "/",
+  public: "./",
+  "symlinks": true,
+  "etag": true
 };
 
-const server = http.createServer((request, response, ) => {
-  return handler(request, response, options);
+
+
+const server = http.createServer((request, response) => {
+  try {
+    return handler(request, response, options).catch((error) => {
+      console.error('Error occurred:', error);
+    }).then((response) => {
+      console.log(`--> ${request.url}`);
+    });
+  } catch (error) {
+    console.log('Error occurred:', error);
+     if(error) return;
+    response.writeHead(500);
+    response.end('Internal Server Error');
+  }
 });
 
 server.listen(PORT, () => {
-  console.log('Running PORT:', PORT);
+  console.log('Running on PORT:', PORT);
 });
